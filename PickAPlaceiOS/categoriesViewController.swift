c4d84a9d591de3d32d263a3402a7c8c78568d8ed
Adam Ding <ding183@purdue.edu>
2019-03-27 17:46:01 -0400

    Added categoriesViewController on the mainStoryBoard. And, created a categoriesViewController.swift to go with the categoriesViewController on the storyboard

---
+++ PickAPlaceiOS/categoriesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  categoriesViewController.swift
+//  PickAPlaceiOS
+//
+//  Created by Adam Ding on 3/27/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+
+//View controller for categoriesViewController on the main storyboard
+
+import UIKit
+
+class categoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
