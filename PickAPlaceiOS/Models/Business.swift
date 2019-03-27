aafd657bbab7fc7bdb39de00b82c229f5f009351
keithatan <tan230@purdue.edu>
2019-03-27 18:59:19 -0400

    Added optionals for Business model

--- PickAPlaceiOS/Models/Business.swift
+++ PickAPlaceiOS/Models/Business.swift
@@ -13,7 +13,7 @@ struct Business: Codable {
     let rating: String
     let image_url: String
     let phone: String
-    let price: String
+    let price: String?
     let url: String
     let location: Location
 }
@@ -23,8 +23,8 @@ struct Location: Codable {
     let city: String
     let country: String
     let address1: String
-    let address2: String
-    let address3: String
+    let address2: String?
+    let address3: String?
     let state: String
     let zip_code: String
 }
