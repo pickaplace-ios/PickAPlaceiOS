875d5efa9564f964c6df2dcbbd307c8c90ed5666
keithatan <tan230@purdue.edu>
2019-03-27 18:01:50 -0400

    Created base Business swift class

---
+++ PickAPlaceiOS/Models/Business.swift
@@ -0,0 +1,19 @@
+//
+//  Business.swift
+//  PickAPlaceiOS
+//
+//  Created by Keith Tan on 3/27/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import Foundation
+
+struct Business: Codable {
+    let name: String
+    let rating: String
+    let image_url: String
+    let phone: String
+    let price: String
+    let url: String
+
+}
