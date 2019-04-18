66d921347713c7c1527211d841c8dc0fb78ff50e
keithatan <tan230@purdue.edu>
2019-04-18 17:40:43 -0400

    Added base for bars category

--- PickAPlaceiOS/Controllers/ChosenViewController.swift
+++ PickAPlaceiOS/Controllers/ChosenViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ChosenViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
     var business = Business(name: "", rating: 0.0, image_url: "", phone: "", price: "", url: "", location: Location(city: "", country: "", address1: "", address2: "", address3: "", state: "", zip_code: ""))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameLabel.text = business.name
+
 
         // Do any additional setup after loading the view.
     }
