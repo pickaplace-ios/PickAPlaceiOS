9b70ee47330442cc4ba4ac3e89bacf79bbed605b
keithatan <tan230@purdue.edu>
2019-04-23 14:53:31 -0400

    UI Design Implementation Phase 4

--- PickAPlaceiOS/Controllers/nearByCell.swift
+++ PickAPlaceiOS/Controllers/nearByCell.swift
@@ -14,11 +14,32 @@ class nearByCell: UITableViewCell {
     
     var business = Business(name: "", rating: 0.0, image_url: "", phone: "", price: "", url: "", location: Location(city: "", country: "", address1: "", address2: "", address3: "", state: "", zip_code: ""), coordinates: BusinessCoordinate(longitude: 0.0, latitude: 0.0), distance: 0.0)
     
+    @IBOutlet weak var tintView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        let rectShape = CAShapeLayer()
+        
+        rectShape.bounds = self.nearByImage.frame
+        rectShape.position = self.nearByImage.center
+        rectShape.path = UIBezierPath(roundedRect: self.nearByImage.bounds,     byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 17, height: 17)).cgPath
+        
+        self.selectionStyle = .none
+
+        
+        
+        self.nearByImage.layer.mask = rectShape
+        
+        let path = UIBezierPath(roundedRect: self.tintView.bounds, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 17, height: 17))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.tintView.bounds
+        maskLayer.path = path.cgPath
+        self.tintView.layer.mask = maskLayer
     }
 
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var nearByImage: UIImageView!
     @IBOutlet weak var nearByNameLabel: UILabel!
     @IBOutlet weak var nearByRating: UILabel!
