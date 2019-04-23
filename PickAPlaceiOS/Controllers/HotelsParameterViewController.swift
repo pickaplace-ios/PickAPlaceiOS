fec681a0592f5e8131438830e57b8f6805e0988b
Adam Ding <ding183@purdue.edu>
2019-04-23 18:00:58 -0400

    Logic Implementation for Parameter View Controllers

--- PickAPlaceiOS/Controllers/HotelsParameterViewController.swift
+++ PickAPlaceiOS/Controllers/HotelsParameterViewController.swift
@@ -13,12 +13,18 @@ class HotelsParameterViewController: UIViewController {
     var params = [String]()
     
     
+    @IBOutlet weak var resortBtn: UIButton!
+    @IBOutlet weak var guesthouseBtn: UIButton!
+    @IBOutlet weak var campBtn: UIButton!
+    @IBOutlet weak var hostelsBtn: UIButton!
+    @IBOutlet weak var bnbBtn: UIButton!
     @IBOutlet weak var priceSegControl: UISegmentedControl!
     
+    @IBOutlet weak var retreatsBtn: UIButton!
+    
+    var types:[String:Bool] = ["resorts":false, "guesthouses":false, "healthretreats":false,  "campgrounds":false, "bedbreakfast":false, "hostels":false]
     override func viewDidLoad() {
         super.viewDidLoad()
-        params.append("categories=hotels&")
-        
         // Do any additional setup after loading the view.
     }
     
@@ -36,13 +42,101 @@ class HotelsParameterViewController: UIViewController {
             params.append("price=1,2,3,4")
         }
     }
+    @IBAction func chooseHostels(_ sender: Any) {
+        if types["hostels"] == false{
+            types["hostels"] = true
+            hostelsBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["hostels"] = false
+            hostelsBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
+    @IBAction func chooseCamp(_ sender: Any) {
+        if types["campgrounds"] == false{
+            types["campgrounds"] = true
+            campBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["campgrounds"] = false
+            campBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
     
+    @IBAction func chooseRetreat(_ sender: Any) {
+        if types["healthretreats"] == false{
+            types["healthretreats"] = true
+            retreatsBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["healthretreats"] = false
+            retreatsBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
+    @IBAction func chooseResort(_ sender: Any) {
+        if types["resorts"] == false{
+            types["resorts"] = true
+            resortBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["resorts"] = false
+            resortBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
+    @IBAction func chooseGuest(_ sender: Any) {
+        if types["guesthouses"] == false{
+            types["guesthouses"] = true
+            guesthouseBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["guesthouses"] = false
+            guesthouseBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
+    @IBAction func chooseBnb(_ sender: Any) {
+        if types["bedbreakfast"] == false{
+            types["bedbreakfast"] = true
+            bnbBtn.setBackgroundImage(UIImage(named: "selectedParameter"), for: .normal)
+        }
+        else {
+            types["bedbreakfast"] = false
+            bnbBtn.setBackgroundImage(UIImage(named: "unselectedParameter"), for: .normal)
+            
+        }
+    }
     
 
     @IBAction func submit(_ sender: Any) {
         grabPrice()
+        grabTypes()
         self.performSegue(withIdentifier: "toRadius", sender: params)
         
+    }
+    
+    func grabTypes(){
+        var category = "categories="
+        var keys = Array(types.keys)
+        
+        for i in 0..<keys.count{
+            if (types[keys[i]] == true){
+                category.append(keys[i])
+                category.append(",")
+            }
+        }
+        
+        if (category == "categories="){
+            category.append("hotels")
+        }
+        else {
+            category = String(category.dropLast())
+        }
+        
+        params.append(category)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
